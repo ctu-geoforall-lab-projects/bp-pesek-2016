85015eeb5c2b64376592d2b22c589eccee746a49
Ondrej Pesek <ondra.lobo@seznam.cz>
2016-02-21 17:15:22 +0100

    Grafika se vstupy a výstupy

--- suro_leveling_dockwidget.py
+++ suro_leveling_dockwidget.py
@@ -22,9 +22,15 @@
 """
 
 import os
+import csv
 
 from PyQt4 import QtGui, uic
-from PyQt4.QtCore import pyqtSignal
+from PyQt4.QtCore import pyqtSignal, QSettings, QTranslator, qVersion, QCoreApplication, Qt
+from PyQt4.QtGui import QAction, QIcon, QDialog, QDialogButtonBox, QFileDialog, QListWidgetItem, QMessageBox
+import resources
+
+#***************************************************************************************************************
+#import move
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'suro_leveling_dockwidget_base.ui'))
@@ -36,6 +42,7 @@ class SuroLevelingDockWidget(QtGui.QDockWidget, FORM_CLASS):
 
     def __init__(self, parent=None):
         """Constructor."""
+
         super(SuroLevelingDockWidget, self).__init__(parent)
         # Set up the user interface from Designer.
         # After setupUI you can access any designer object by doing
@@ -43,6 +50,44 @@ class SuroLevelingDockWidget(QtGui.QDockWidget, FORM_CLASS):
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
+
+        self.inputButton.clicked.connect(self.select_input)
+        self.outputButton.clicked.connect(self.select_output)
+
+        self.input.textChanged.connect(self.ablesolve) # enable of Solve button
+        self.output.textChanged.connect(self.ablesolve)
+        self.value.textChanged.connect(self.ablesolve)
+        #***************************************************************************************************
+        #self.solve.clicked.connect(move.move_by_points)
+
+    def select_input(self):
+        """select .csv file to edit"""
+
+        self.filepath = QFileDialog.getOpenFileName(self, 'Load file','.', 'Comma Seperated Values (*.csv)')
+
+        if not self.filepath:
+            return
+
+        self.input.setText(self.filepath)
+        self.output.setText(self.filepath[:-4]+u'_leveled.csv')
+
+    def select_output(self):
+        """choose directory to save returned data"""
+
+        self.directory = QFileDialog.getExistingDirectory(self, 'Save to file') + u'\leveled_data.csv'
+        if not self.directory:
+            return
+
+        self.output.setText(self.directory) #** divna lomitka * musi se zmenit na jmeno souboru ***********************
+
+    def ablesolve(self):
+        """set Solve button enable"""
+
+        if self.input.text() and self.output.text() and self.value.text():
+            self.solve.setEnabled(True)
+        else:
+            self.solve.setEnabled(False)
+
 
     def closeEvent(self, event):
         self.closingPlugin.emit()
